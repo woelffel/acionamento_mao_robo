809ed08b4b612dfb71cca9b12da53d32354efa90
woelffel <marlonwoelffel@gmail.com>
2019-10-25 20:48:32 -0300

    Update comunicacao_pc.py

--- Comunicacao_serial_arduino/comunicacao_pc.py
+++ Comunicacao_serial_arduino/comunicacao_pc.py
@@ -1,28 +1,32 @@
 import serial
 
-#### Funcao para abrir conexao serial com a porta desejada
-def abrir_conexao(porta):
-    #### Comecar comunicacao serial
-    porta_usb = "USB0"
-    conexao = serial.Serial('/dev/' + porta_usb, 9600)
+class arduino():
 
-    #### Averiguando se conexao relamente esta aberta
-    status_conexao = False
-    if (conexao.isOpen()):
-        status_conexao = True
-    
-    return (conexao, status_conexao)
+    def __init__(self):
+        self.nada = 0
+    #### Funcao para abrir conexao serial com a porta desejada
+    def abrir_conexao(porta):
+        #### Comecar comunicacao serial
+        conexao = serial.Serial('/' + porta, 9600)
 
-#### Funcao para enviar o comando
-def envia_comando(conexao, comando):
-    '''
-    Entradas:
-        Conexao é o que retorna da funcao "abrir_conexao"
-        Comando é um comando que vai de 1 a 5 que representa
-            as classes de cada saída da rede
-    Saída:
-        Sem retorno
-    '''
-    #### Preparando mensagem
-    mensagem = ":" + str(comando) + ":"
-    conexao.write(mensagem)
+        #### Averiguando se conexao relamente esta aberta
+        status_conexao = False
+        if (conexao.isOpen()):
+            status_conexao = True
+        
+        return (conexao, status_conexao)
+
+
+    #### Funcao para enviar o comando
+    def envia_comando(conexao, comando):
+        '''
+        Entradas:
+            Conexao é o que retorna da funcao "abrir_conexao"
+            Comando é um comando que vai de 1 a 5 que representa
+                as classes de cada saída da rede
+        Saída:
+            Sem retorno
+        '''
+        #### Preparando mensagem
+        mensagem = str(comando)
+        conexao.write(mensagem.encode('utf-8'))
