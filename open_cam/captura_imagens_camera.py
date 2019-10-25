f1c7015fd7069ec25108e2574350f5aebdd5869e
woelffel <marlonwoelffel@gmail.com>
2019-10-25 20:49:47 -0300

    Update captura_imagens_camera.py

--- open_cam/captura_imagens_camera.py
+++ open_cam/captura_imagens_camera.py
@@ -1,46 +1,52 @@
 import numpy as np
 import cv2
 
-### Inicializa a camera e retorna uma instancia da mesma
-def inicializacao_camera():
-    camera = cv2.VideoCapture(0)
-    return camera
+class opencv():
+    def __init__(self):
+        self.nada = 0
 
-def captura_frame_gray():
-    # Capture frame-by-frame
-    ret, frame = camera.read()
+    ### Inicializa a camera e retorna uma instancia da mesma
+    def init_camera():
+        camera = cv2.VideoCapture(0)
+        return camera
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    def captura_frame_gray(camera):
+        # Capture frame-by-frame
+        ret, frame = camera.read()
 
-    return gray
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-def show_image(frame, comando):
-    ### Display the resulting frame
-    ## Escrevendo o comando na tela
-    font                   = cv2.FONT_HERSHEY_SIMPLEX
-    bottomLeftCornerOfText = (10,500)
-    fontScale              = 3
-    fontColor              = (255,255,255)
-    lineType               = 2
+        return gray
 
-    cv2.putText(img, str(comando), 
-        bottomLeftCornerOfText, 
-        font, 
-        fontScale,
-        fontColor,
-        lineType)
-    
-    ## Mostrando a imagem com o texto
-    cv2.imshow('frame',frame)
-    '''
-    Utilizar essa linha de código para quebrar o "while" que vai ter para
-    capturar as imagens constantemente 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    '''
+    def show_image(frame, comando):
+        ### Display the resulting frame
+        ## Escrevendo o comando na tela
+        # font                   = cv2.FONT_HERSHEY_SIMPLEX
+        # bottomLeftCornerOfText = (10,500)
+        # fontScale              = 3
+        # fontColor              = (255,255,255)
+        # lineType               = 2
 
-def finalizar_camera():
-    # When everything done, release the capture
-    camera.release()
-    cv2.destroyAllWindows()
+        # cv2.putText(img, str(comando), 
+        #     bottomLeftCornerOfText, 
+        #     font, 
+        #     fontScale,
+        #     fontColor,
+        #     lineType)
+        
+        ## Mostrando a imagem com o texto
+        cv2.imshow('frame',frame)
+        # cv2.waitKey(0)
+        # cv2.destroyWindow()
+        '''
+        Utilizar essa linha de código para quebrar o "while" que vai ter para
+        capturar as imagens constantemente 
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        '''
+
+    def finalizar_camera(camera):
+        # When everything done, release the capture
+        camera.release()
+        cv2.destroyAllWindows()
